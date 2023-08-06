d3822bdee0fb548bb5957304ab189fde809ffa42
Aanshik <aanshik16@gmail.com>
2023-08-06 05:49:57 +0530

    Styled the contact area and added the modified JS file
    I am not very good at writing JavaScript codes that is why it is going to take a little while before I can finish this project.

--- script.js
+++ script.js
@@ -3,9 +3,22 @@
 
 let finalRating = document.getElementById('numeric-rating');
 
+let sidebar = document.getElementsByClassName('sidebar');
+function toggleNavbar() {
+    if (sidebar[0].style.display !== 'none') {
+        sidebar[0].style.display = 'none';
+    } else {
+        sidebar[0].style.display = 'block';
+    }
+}
 
-
-
+window.addEventListener("click", (element) => {
+    if (element !== sidebar) {
+        if (sidebar[0].style.display !== 'none') {
+            sidebar[0].style.display = 'none';
+        }
+    }
+});
 
 
 
